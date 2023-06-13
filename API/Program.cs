f5f17a99167ce05040bd3b9c235ba9aebcefc88e
n.asadipour <n.asadipour@charisma.ir>
2023-06-13 14:44:24 +0330

    End of section 4. add Reactivities mediator handlers for create, edit, delete, details and list in application. add mappingProfiles

--- API/Program.cs
+++ API/Program.cs
@@ -1,25 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt => 
-{
-    opt.AddPolicy("CorsPolicy", policy => 
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationService(builder.Configuration);
 
 var app = builder.Build();
 
--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Reactivities
 learning Complete guide to building an app with .Net Core and React
 from Neil Cummings . coures link : https://www.udemy.com/course/complete-guide-to-building-an-app-with-net-core-and-react/
 
-using:
+in backend api using:
 
+.net 7
+clean architecture
+CQRS + Mediator pattern 
+
+
+in frontend using:
+
+react + typescript
 axios
 react.semantic-ui
