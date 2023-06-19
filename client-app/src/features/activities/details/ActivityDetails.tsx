ca2ac2970fd2b3a46c89157d7d908259811d8542
n.asadipour <n.asadipour@charisma.ir>
2023-06-19 11:20:33 +0330

    End of section 8. add routing and edit and create functionality

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,13 +1,22 @@
 import { Button, Card, Image } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { observer } from "mobx-react-lite";
+import { Link, useParams } from "react-router-dom";
+import { useEffect } from "react";
 
-export default function ActivityDetails() {
+export default observer(function ActivityDetails() {
 
     const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams();
 
-    if (!activity) return <LoadingComponent content="no activity ..."/>;
+    useEffect(() => {
+        if (id) loadActivity(id);
+    
+    }, [id, loadActivity])
+
+    if (loadingInitial || !activity) return <LoadingComponent content="no activity ..."/>;
 
     return (
         <Card fluid>
@@ -23,10 +32,10 @@ export default function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color="blue" content='Edit' />
-                    <Button onClick={cancelSelectedActivity} basic color="grey" content='Cancel' />
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content='Edit' />
+                    <Button as={Link} to='/activities' basic color="grey" content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
     )
-}
+})
